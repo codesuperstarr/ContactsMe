ae91e0322ff0fc3c45a1e2fbc19e9860148ba63e
Divine Ugorji <divineugorji37@gmail.com>
2020-11-16 16:03:29 +0100

    initial commit

--- app/src/main/java/com/example/contactsme/ContactsMeActivity.kt
+++ app/src/main/java/com/example/contactsme/ContactsMeActivity.kt
@@ -2,11 +2,18 @@ package com.example.contactsme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.contactsme.adapters.ViewPagerAdapter
 
-class MainActivity : AppCompatActivity() {
+class ContactsMeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupTabs()
+    }
+
+    private fun setupTabs() {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
     }
 
 }
