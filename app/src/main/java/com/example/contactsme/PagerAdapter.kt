0221315c79d825fcae9af95b53e88d9b008dc00e
Divine Ugorji <divineugorji37@gmail.com>
2020-11-16 15:38:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/contactsme/PagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.contactsme
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+/*class PagerAdapter(fm: FragmentManager?, var mNumOfTabs: Int) :
+    FragmentStatePagerAdapter(fm!!) {
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+   /* override fun getItem(position: Int): Fragment {
+        return null
+    }*/
+
+    /**
+     * Return the number of views available.
+     */
+    override fun getCount(): Int {
+        return 0
+    }
+
+}*/
